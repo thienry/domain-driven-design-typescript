665a4c3b88ad8075d7842161e55ecd8d7ebef7eb
thienry <thmoura14@hotmail.com>
2022-07-31 13:30:16 -0300

    refac: refactor jest and swc type error

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ const address = new Address('Street', 1, 'City', 'Zipcode')
 customer.changeAddress(address)
 customer.activate()
 
-const item1 = new OrderItem('1', 'Item 1', 10)
-const item2 = new OrderItem('2', 'Item 2', 15)
-const order = new Order('1', customer.getId(), [item1, item2])
+const item1 = new OrderItem('123', 'Item 1', 10, 2, '123')
+const item2 = new OrderItem('456', 'Item 2', 30, 2, '456')
+const order = new Order('123', 0, customer.id, [item1, item2])
 
 console.log(JSON.stringify(order.toString(), null, 2))
